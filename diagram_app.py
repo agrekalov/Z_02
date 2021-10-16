ba3966ac1d4ad1c9d21f93e3963c4f8aceb1488a
Aleksandr Grekalov <agrekalov@gmail.com>
2021-10-16 23:40:11 +0300

    Added file diagram

--- diagram_app.py
+++ diagram_app.py
@@ -1,5 +1,8 @@
+# diagram_app.py
 from diagrams import Diagram
 from diagrams.aws.compute import EC2
+from diagrams.aws.database import RDS
+from diagrams.aws.network import ELB
 
-with Diagram("Simple Diagram", outformat="jpg"):
-    EC2("web")
+with Diagram("Web Service", show=False):
+    ELB("lb") >> EC2("web") >> RDS("userdb")
